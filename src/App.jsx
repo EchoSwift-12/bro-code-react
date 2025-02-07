8d080397cf0b9a7cd17ecda7113937aeb5d6090a
[EchoSwift-12] <[amtulrahmansadiya@gmail.com]>
2025-02-07 15:20:25 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+// import Card from "./Card";
+// import Student from "./Student.jsx";
+// import ChildComponent from "./ChildCom.jsx";
+// import List from "./List.jsx";
+// import ArrOfObjs from "./List.jsx"
+// import Button from "./Button.jsx";
+// import Button2 from "./Button2.jsx";
+// import ProfilePicture from "./ProfilePicture.jsx";
+// import MyComponent from "./MyComponent.jsx";
+// import Counter from "./Counter.jsx";
+// import Mycomponent2 from "./Mycomponent2.jsx";
+// import ColorPicker from "./ColorPicker.jsx";
+// import CounterUpdate from "./Update.jsx"
+// import UpdateObj from "./UpdateObj";
+// import UpdateArray from "./UpdateArray"
+// import UpdateArrOfObj from "./UpdateArrOfObj"
+import ToDoList from "./todoList"
+
+const App = () => {
+
+  return (
+    <div className="app">
+      {/* <Card /> */}
+      {/* <Student name="John Doe" age= {20} isStudent ={true} />
+      <Student name="carle" age= {90} isStudent ={false}/>
+      <Student name="Joh" age={40} isStudent ={false}/>
+      <ChildComponent greeting="Hello" name="John Doe" /> */}
+      {/* <List/> */}
+      {/* <ArrOfObjs/> */}
+      {/* <Button/> */}
+      {/* <Button2/> */}
+      {/* <ProfilePicture /> */}
+      {/* <MyComponent/> */}
+      {/* <Counter/> */}
+      {/* <Mycomponent2/> */}
+      {/* <ColorPicker/> */}
+      {/* <CounterUpdate/> */}
+      {/* <UpdateObj/> */}
+      {/* <UpdateArray/> */}
+      {/* <UpdateArrOfObj /> */}
+      <ToDoList/>
+    </div>
+
+  );
+}
+
+export default App;
+
+
